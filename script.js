e939be16fcfb08e6ad5319d2da028e4dceea24a8
guilherme melo <guilhermemelodecirqueira@gmail.com>
2023-05-12 10:41:18 -0300

    segunda versão calculadora

--- script.js
+++ script.js
@@ -1,122 +1,138 @@
-let inputBase = document.querySelector(".inputBase")
-let inputExpoente = document.querySelector(".inputExpoente")
-let iconLimoar = document.querySelector(".input_valor i")
-
-
-// variaveis de botão
-
-let bottonPotencicao = document.querySelector(".botão_potencicao")
-
-let bottonSoma = document.querySelector(".botao_soma")
-
-let bottoSuntracao = document.querySelector(".botao_subtracao")
-
-let botaoMultiplicacao = document.querySelector(".botao_multiplicacao")
-
-let botaoDivisao = document.querySelector(".bottao_divisao")
-
-// funcões de calculo
-
-function potencição(){
-    const baseValor =  Number(inputBase.value)
-
-    const ValorExpoente =  Number(inputExpoente.value)
-
-    let resultado =  document.createElement("p")
-    
-    inputExpoente.after(resultado)
-
-    resultado.innerText = `o resultado da potenciação e ${Math.pow(baseValor , ValorExpoente)}`
-
-    bottonPotencicao.addEventListener("click" , function zerar(){
-        resultado.innerText = ""
-    }) 
-    
-    iconLimoar.addEventListener("click" , function(){
-        resultado.innerText = ""
-     })
+let qd = (x) =>{
+    return document.querySelector(x)
 }
 
-function soma(){
-    const baseValor =  Number(inputBase.value)
+// variaveis das operações
+let somaButtom = qd(".soma")
 
-    const ValorExpoente =  Number(inputExpoente.value)
+let subtracaoButton = qd(".subtração")
 
-    let resultado =  document.createElement("p")
+let multplicacaoButton = qd(".multiplicaçao")
 
-    inputExpoente.after(resultado)
+let divisaoButton = qd(".divisao")
 
-    resultado.innerText = `o resultado da soma e ${baseValor + ValorExpoente}`
+let potenciacaoButton = qd(".potenciacao")
 
-    bottonSoma.addEventListener("click" , function zerar(){
-        resultado.innerText = ""
+let radiacaoButton = qd(".radiacao") 
+
+// class inputs
+
+let input1Operaacao =   qd(".input1")
+let input2Operaacao =   qd(".input2")
+
+// variaveis das classs da classes
+
+let divInputs = qd(".inputOperacao")
+
+let buttonResult = qd(".resultado")
+
+// variaveis dos resultados das operações
+
+let resultadoOperacao = document.createElement("p")
+input2Operaacao.after(resultadoOperacao)
+
+
+
+// funcões e eventos
+somaButtom.addEventListener("click" , () => {
+
+    resultadoOperacao.innerHTML = ""
+    input1Operaacao.setAttribute("placeholder" ,"digite um valor para soma" )
+    input2Operaacao.setAttribute("placeholder" ,"digite um valor para soma" )
+    divInputs.setAttribute("class" , "inputOperacaoJs")
+    input2Operaacao.style.display = "block"
+
+    buttonResult.addEventListener("click" , () => {
+        let soma =   Number( input1Operaacao.value) + Number( input2Operaacao.value) 
+        resultadoOperacao.innerHTML = soma
+        // input1Operaacao.value = ""
+        // input2Operaacao.value = ""
+    })
+})
+
+
+subtracaoButton.addEventListener("click" , () => {
+    
+    resultadoOperacao.innerHTML = ""
+    divInputs.setAttribute("class" , "inputOperacaoJs")
+    input1Operaacao.setAttribute("placeholder" ,"digite um minuendo" )
+    input2Operaacao.setAttribute("placeholder" ,"digite um subtraendo" )
+    input2Operaacao.style.display = "block"
+
+    buttonResult.addEventListener("click" , () => {
+        let subtracao =   Number( input1Operaacao.value) - Number( input2Operaacao.value) 
+        resultadoOperacao.innerHTML = subtracao
+        // input1Operaacao.value = ""
+        // input2Operaacao.value = ""
     })
 
-    iconLimoar.addEventListener("click" , function(){
-        resultado.innerText = ""
-     })
-   
-}
-function sobtracao(){
-    const baseValor =  Number(inputBase.value)
 
-    const ValorExpoente =  Number(inputExpoente.value)
+})
 
-    let resultado =  document.createElement("p")
+multplicacaoButton.addEventListener("click" , () => {
 
-    inputExpoente.after(resultado)
+    resultadoOperacao.innerHTML = ""
+    divInputs.setAttribute("class" , "inputOperacaoJs")
+    input1Operaacao.setAttribute("placeholder" ,"digite um fator" )
+    input2Operaacao.setAttribute("placeholder" ,"digite um fator" )
+    input2Operaacao.style.display = "block"
 
-    resultado.innerText =`o resultado da subtração e ${baseValor - ValorExpoente}`
-
-    bottoSuntracao.addEventListener("click" , function zerar(){
-        resultado.innerText = ""
-    }) 
-
-    iconLimoar.addEventListener("click" , function(){
-        resultado.innerText = ""
-     })
-}
+    buttonResult.addEventListener("click" , () => {
+        let multiplicacao =   Number( input1Operaacao.value) * Number( input2Operaacao.value) 
+        resultadoOperacao.innerHTML = multiplicacao
+        // input1Operaacao.value = ""
+        // input2Operaacao.value = ""
+    })
+})
 
 
-function multiplicacao(){
-    const baseValor =  Number(inputBase.value)
+divisaoButton.addEventListener("click" , () => {
 
-    const ValorExpoente =  Number(inputExpoente.value)
+    resultadoOperacao.innerHTML = ""
+    divInputs.setAttribute("class" , "inputOperacaoJs")
+    input1Operaacao.setAttribute("placeholder" ,"digite um dividendo" )
+    input2Operaacao.setAttribute("placeholder" ,"digite um divisor" )
+    input2Operaacao.style.display = "block"
 
-    let resultado =  document.createElement("p")
-
-    inputExpoente.after(resultado)
-
-    resultado.innerText = `o resultado da multiplicação e ${baseValor * ValorExpoente}`
-
-    botaoMultiplicacao.addEventListener("click" , function zerar(){
-        resultado.innerText = ""
-    }) 
-
-    iconLimoar.addEventListener("click" , function(){
-        resultado.innerText = ""
-     })
-}
-
-function divisao(){
+    buttonResult.addEventListener("click" , () => {
+        let divisao =   Number( input1Operaacao.value) / Number( input2Operaacao.value) 
+        resultadoOperacao.innerHTML = divisao
+        if (Number.isNaN(divisao)){
+            resultadoOperacao.innerHTML = "não foi possivel realizar essa operacão"
+        }
+        // input1Operaacao.value = ""
+        // input2Operaacao.value = ""
+    })
     
-    const baseValor =  Number(inputBase.value)
+})
 
-    const ValorExpoente =  Number(inputExpoente.value)
+potenciacaoButton.addEventListener("click" , () => {
+ 
+    resultadoOperacao.innerHTML = ""
+    divInputs.setAttribute("class" , "inputOperacaoJs")
+    input1Operaacao.setAttribute("placeholder" ,"digite uma base" )
+    input2Operaacao.setAttribute("placeholder" ,"digite um expoente" )
+    input2Operaacao.style.display = "block"
 
-    let resultado =  document.createElement("p")
+    buttonResult.addEventListener("click" , () => {
+        let poteciacao =  Math.pow( Number( input1Operaacao.value) , Number( input2Operaacao.value) )
+        resultadoOperacao.innerHTML = poteciacao
+        // input1Operaacao.value = ""
+        // input2Operaacao.value = ""
+    })
+})
 
-    inputExpoente.after(resultado)
+radiacaoButton.addEventListener("click" , () => {
 
-    resultado.innerText = `o resultado da divisão e ${baseValor / ValorExpoente}`
-
-    botaoDivisao.addEventListener("click" , function zerar(){
-        resultado.innerText = ""
-    }) 
-
-    iconLimoar.addEventListener("click" , function(){
-        resultado.innerText = ""
-     })
+    resultadoOperacao.innerHTML = ""
+    divInputs.setAttribute("class" , "inputOperacaoJs")
+    input1Operaacao.setAttribute("placeholder" ,"digite uma radicando" )
+    input2Operaacao.style.display = "none"
     
-}
-
+    buttonResult.addEventListener("click" , () => {
+        let radiacao =  Math.sqrt(Number( input1Operaacao.value))
+        resultadoOperacao.innerHTML = radiacao
+        // input1Operaacao.value = ""
+        // input2Operaacao.value = ""
+   })
+})
